9d8e1fadc4344087a8431b7717b119a8cd774953
sooryansh77 <sooryansh777@gmail.com>
2025-08-21 20:09:10 +0530

    Added Q21

--- src/Patterns.java
+++ src/Patterns.java
@@ -351,9 +351,24 @@ public class Patterns {
 
     }
 
+    public static void Q21(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(i==1 || i == n || j == 1 || j ==n){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+
 
 
     public static void main(String[] args) {
-        Q20_alternate(5);
+        Q21(4);
     }
 }
